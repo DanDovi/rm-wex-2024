3ef63c497e85122a041db1af0ffe6faa0adbdec9
rlucaks <154709325+rlucaks@users.noreply.github.com>
2024-07-10 15:21:52 +0100

    Sort posts by most recent and take only top  50

--- apps/api/src/services/post.ts
+++ apps/api/src/services/post.ts
@@ -5,7 +5,8 @@ class postService {
     const prisma = new PrismaClient();
     const posts = await prisma.post.findMany();
     prisma.$disconnect();
-    return posts;
+    posts.slice(0, 50);
+    return posts.sort((a, b) => a.createdAt.getTime() - b.createdAt.getTime());
   }
 }
 export { postService };

d1b6ed6301f5e643733c2b689174829e06f290d4
rlucaks <154709325+rlucaks@users.noreply.github.com>
2024-07-11 15:57:27 +0100

    fixed small issues with missing files

--- apps/api/src/routes/post.ts
+++ apps/api/src/routes/post.ts
@@ -8,5 +8,6 @@ const router = Router();
 router.get("/", authMiddleware, postController.allPosts);
 router.get("/:id", authMiddleware, postController.postById);
 router.post("/:topicId/posts", authMiddleware, postController.createPost);
+router.post("/:id/vote");
 
 export default router;

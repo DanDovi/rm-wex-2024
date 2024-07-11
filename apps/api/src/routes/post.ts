0bd4ec9b3df24fc109b9e21149c48e455d81f87b
rlucaks <154709325+rlucaks@users.noreply.github.com>
2024-07-11 15:59:40 +0100

    Added authmiddleware

--- apps/api/src/routes/post.ts
+++ apps/api/src/routes/post.ts
@@ -2,12 +2,13 @@ import { Router } from "express";
 
 import { postController } from "../controllers/post";
 import { authMiddleware } from "../middleware/auth";
+import { postService } from "../services/post";
 
 const router = Router();
 
 router.get("/", authMiddleware, postController.allPosts);
 router.get("/:id", authMiddleware, postController.postById);
 router.post("/:topicId/posts", authMiddleware, postController.createPost);
-router.post("/:id/vote");
+router.post("/:id/vote", authMiddleware, postService.updatePostVote);
 
 export default router;

afdb13d77f6a49a4af2cb3eeb8fae8026e653a26
Megan Power <meganpower242@gmail.com>
2024-07-11 15:36:16 +0100

    added create comment

--- apps/api/src/routes/post.ts
+++ apps/api/src/routes/post.ts
@@ -8,5 +8,6 @@ const router = Router();
 router.get("/", authMiddleware, postController.allPosts);
 router.get("/:id", authMiddleware, postController.postById);
 router.post("/:topicId/posts", authMiddleware, postController.createPost);
+router.post("/:id/comments", authMiddleware, postController.newComment);
 
 export default router;

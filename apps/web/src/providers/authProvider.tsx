ce15d6d2eca83062fc9e40046be916297afdf2b3
Dan Dando <dan@rocketmakers.com>
2024-07-12 09:36:09 +0100

    added helpers for getting user details from local storage

--- apps/web/src/providers/authProvider.tsx
+++ apps/web/src/providers/authProvider.tsx
@@ -3,6 +3,7 @@ import { useNavigate } from "react-router-dom";
 
 import { login as loginUser, register as registerUser } from "../api/auth";
 import { authContext, getUserFromStorage } from "../hooks/useAuth";
+import { IUserDetails, storeUserDetails } from "../utils/authToken";
 
 export const AuthProvider: React.FC<React.PropsWithChildren> = ({
   children,
@@ -16,8 +17,10 @@ export const AuthProvider: React.FC<React.PropsWithChildren> = ({
       username,
       password,
     );
-    const storedUser = JSON.stringify({ username: resUserName, accessToken, userId });
-    localStorage.setItem("token", storedUser);
+
+    const userDetails: IUserDetails = { username: resUserName, accessToken, userId };
+
+    storeUserDetails(userDetails);
     setUser({ username: resUserName, userId });
     navigate("/app/dashboard");
   };
@@ -33,8 +36,10 @@ export const AuthProvider: React.FC<React.PropsWithChildren> = ({
       username,
       password,
     );
-    const storedUser = JSON.stringify({ username: resUserName, accessToken, userId });
-    localStorage.setItem("token", storedUser);
+
+    const userDetails: IUserDetails = { username: resUserName, accessToken, userId };
+
+    storeUserDetails(userDetails);
     setUser({ username: resUserName, userId });
     navigate("/app/dashboard");
   };

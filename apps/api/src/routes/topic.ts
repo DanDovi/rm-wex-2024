a31c74bc5235bbf72a69881b7211ca47266ca1b5
rlucaks <154709325+rlucaks@users.noreply.github.com>
2024-07-12 10:57:25 +0100

    fixed incorrect html request

--- apps/api/src/routes/topic.ts
+++ apps/api/src/routes/topic.ts
@@ -7,7 +7,7 @@ const router = Router();
 
 router.get("/", authMiddleware, topicController.allTopics);
 router.get("/:id", authMiddleware, topicController.topicById);
-router.post("/:topicId/posts", authMiddleware, topicController.postsByTopicId);
+router.get("/:topicId/posts", authMiddleware, topicController.postsByTopicId);
 router.post("/create", authMiddleware, topicController.createTopic);
 
 export default router;
